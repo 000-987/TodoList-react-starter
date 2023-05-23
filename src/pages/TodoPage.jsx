5c94eaae18817abd1876736cfe4bd355e9020f55
000-987 <usphadkfj159@gmail.com>
2023-05-23 22:14:49 +0800

    切換編輯模式

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -82,6 +82,36 @@ const TodoPage = () => {
       });
     });
   };
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          };
+        }
+
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+  const handleSave = ({ id, title }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            id,
+            title,
+            isEdit: false,
+          };
+        }
+
+        return todo;
+      });
+    });
+  };
   return (
     <div>
       TodoPage
@@ -92,7 +122,12 @@ const TodoPage = () => {
         onAddTodo={handleAddTodo}
         onKeyDown={handleKeyDown}
       />
-      <TodoCollection todos={todos} onToggleDone={handleToggleDone} />
+      <TodoCollection
+        todos={todos}
+        onSave={handleSave}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+      />
       <Footer />
     </div>
   );
